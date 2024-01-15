3dd27e3803a211d4f009aaedc2d3f8d83ff1871b
Erik <lambrechtserik@hotmail.com>
2024-01-15 20:38:16 +0100

    p5 base

--- base_p5_svg/sketch.js
+++ base_p5_svg/sketch.js
@@ -1,18 +1,12 @@
-
 function setup() {
   createCanvas(200, 200, SVG); // Create SVG Canvas
-  strokeWeight(1); // do 0.1 for laser
-  stroke(255, 0, 0); // red is good for laser
-  noFill(); // better not to have a fill for laser
 }
 
 function draw() {
-  for (x = 0; x < 10; x++) {
-    for (y = 0; y < 10; y++) {
-      rect(x * 10, y * 10, x, y);
-    }
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
   }
-  save("mySVG.svg"); // give file name
-  print("saved svg");
-  noLoop(); // we just want to export once
+  ellipse(mouseX, mouseY, 80, 80);
 }
